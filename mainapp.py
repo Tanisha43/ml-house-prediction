bd93474015a8e3d28988cbf8afbb21956d64173c
Tanisha43 <60284861+Tanisha43@users.noreply.github.com>
2020-07-02 11:59:42 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -21,8 +21,9 @@ def predict():
 	#output = round(prediction[0],2)
 	return render_template('index.html',prediction_text="Price of house Should be {}".format(math.floor(prediction)))
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
 
-app.run(debug=True)
 
 
 
