a4ecda8bacad7765028b0409c6bd439c2c673fb9
Tanisha43 <60284861+Tanisha43@users.noreply.github.com>
2020-07-02 11:55:00 +0530

    Add files via upload

---
+++ mainapp.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import math
+
+app = Flask(__name__)
+model = pickle.load(open('home.pkl','rb'))
+
+@app.route("/")
+def index():
+
+    return render_template('index.html')
+
+
+@app.route("/predict",methods=['POST'])
+def predict():
+
+	int_features =[int(x) for x in request.form.values()]
+	final_features = [np.array(int_features)]
+	prediction = model.predict(final_features)
+	#output = round(prediction[0],2)
+	return render_template('index.html',prediction_text="Price of house Should be {}".format(math.floor(prediction)))
+
+
+app.run(debug=True)
+
+
+
+
+
+
+
+
